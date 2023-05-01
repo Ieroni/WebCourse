c9b7e39e3769a26fcb1ef02d8b0efb77b0b3fe6a
Ieroni <111338634+Ieroni@users.noreply.github.com>
2023-05-01 13:45:18 +0300

    fixing after teachers notes

--- HomeWork/3th_Seminar/page1.js
+++ HomeWork/3th_Seminar/page1.js
@@ -10,24 +10,26 @@
 // const num3 = Number.parseFloat(prompt("введите 2jе числов "));
 
 // alert(`сложение ${num2} and ${num3} is ${num2 + num3}.`);
-const celc = prompt("температуру в градусах Цельсия ");
-	const  faren = ((9/5)*celc+32).toFixed(1); 
-	alert(`температуру в Фаренгейтах равно ${faren}`);
+const celc = Number.parseFloat(prompt("температуру в градусах Цельсия "));
+const faren = ((9 / 5) * celc + 32);
+const farenhegt = (Math.floor(faren*100))/100;
+alert(`температуру в Фаренгейтах равно ${farenhegt}`);
 
-function openWindow2() {
-	window.location.href = 'page2.html';
-	// window.open("page2.html", "page2", "width=500,height=500");
-}
+const goToPage2Btn = document.querySelector('.go_to_page2_btn');
+goToPage2Btn.addEventListener('click', () => {
+  window.location.href = 'page2.html';
+});
 
-window.onload = function() {
-    var container = document.querySelector('.container');
-    var contentHeight = container.clientHeight;
-    var windowHeight = window.innerHeight;
-    var scrollHeight = contentHeight - windowHeight;
-  
-    window.addEventListener('scroll', function() {
-      var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-      var scrollPercentage = scrollPosition / scrollHeight * 100;
-      container.style.transform = 'translateY(-' + scrollPercentage + '%)';
-    });
+
+window.onload = function () {
+  let container = document.querySelector('.container');
+  let contentHeight = container.clientHeight;
+  let windowHeight = window.innerHeight;
+  let scrollHeight = contentHeight - windowHeight;
+
+  window.addEventListener('scroll', function () {
+    let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollPercentage = scrollPosition / scrollHeight * 100;
+    container.style.transform = 'translateY(-' + scrollPercentage + '%)';
+  });
 };
